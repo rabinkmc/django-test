21cc432540053ed5ff579d2a319d43a547cb4f9f
Rabin Dhamala <rabinkmc@gmail.com>
2023-03-12 20:57:03 +0545

    add rest_framework

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view()
+def index(_):
+    return Response("index page.")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index)
 ]
